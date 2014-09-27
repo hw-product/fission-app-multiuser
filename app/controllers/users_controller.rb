9c644b981964680885f086c093fc846d43d45476
Chris Roberts <chrisroberts.code@gmail.com>
2014-09-27 11:24:39 -0700

    Remove deprecated constant

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,6 @@ class UsersController < ApplicationController
 
   before_action :validate_user!, :except => [:new, :create]
 
-  include BasicCrud
-
   def new
     respond_to do |format|
       format.html do
