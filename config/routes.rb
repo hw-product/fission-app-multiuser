f7a68b51fef62c877340b2cd3a73941f02a02609
Chris Roberts <chrisroberts.code@gmail.com>
2014-12-03 08:11:59 -0800

    Add routes for product feature modifications on accounts

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :accounts do
+      post 'product_feature/:product_feature_id', :to => 'accounts#add_product_feature', :as => :add_product_feature
+      delete 'product_feature/:product_feature_id', :to => 'accounts#remove_product_feature', :as => :remove_product_feature
       resources :users
       resources :permissions
       resources :tokens
-      resources :product_features
     end
     resources :users do
       resources :tokens
