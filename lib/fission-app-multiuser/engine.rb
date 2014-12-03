a8fe99ba38a306d17f904c5048e25610d692dfdf
Chris Roberts <chrisroberts.code@gmail.com>
2014-12-03 05:35:16 -0800

    Add admin navigation

--- lib/fission-app-multiuser/engine.rb
+++ lib/fission-app-multiuser/engine.rb
@@ -5,6 +5,29 @@ module FissionApp
       config.to_prepare do |config|
       end
 
+      # @return [Array<Fission::Data::Models::Product>]
+      def fission_product
+        [Fission::Data::Models::Product.find_by_internal_name('fission')]
+      end
+
+      # @return [Hash] navigation
+      def fission_navigation
+        Smash.new(
+          'Admin' => Smash.new(
+            'Sources' => Rails.application.routes.url_for(
+              :controller => 'admin/sources',
+              :action => :index,
+              :only_path => true
+            ),
+            'Accounts' => Rails.application.routes.url_for(
+              :controller => 'admin/accounts',
+              :action => :index,
+              :only_path => true
+            )
+          )
+        )
+      end
+
     end
   end
 end
