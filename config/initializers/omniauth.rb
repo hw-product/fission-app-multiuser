509c5d4913cde93323a55b6066b4ad3435b660a7
Chris Roberts <chrisroberts.code@gmail.com>
2015-02-06 06:19:36 -0800

    If no mapping can be located for github configuration, fallback to default key

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -13,6 +13,9 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     :github,
     :setup => lambda {|env|
       host_key = env['SERVER_NAME']
+      unless(Rails.application.config.fission.github[host_key])
+        host_key = 'default'
+      end
       env['omniauth.strategy'].options[:client_id] = Rails.application.config.fission.github[host_key][:key]
       env['omniauth.strategy'].options[:client_secret] = Rails.application.config.fission.github[host_key][:secret]
       env['omniauth.strategy'].options[:scope] = 'user:email,repo'
