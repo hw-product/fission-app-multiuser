904c20129cf5fe4c884bc2f8bc062be93f0e62a9
Chris Roberts <code@chrisroberts.org>
2015-06-03 14:52:55 -0700

    Add dynamic styling generator

---
+++ lib/fission-app-multiuser/styler.rb
@@ -0,0 +1,58 @@
+require 'fission-app-multiuser'
+
+module FissionApp
+  module Multiuser
+    # Style generator
+    class Styler
+
+      include Bogo::Memoization
+
+      # @return [String] name of product
+      attr_reader :product_name
+      # @return [Hash] key value pair overrides for theme
+      attr_reader :overrides
+
+      # Create new instance
+      #
+      # @param product_name [String]
+      # @param overrides [Hash]
+      # @return [self]
+      def initialize(product_name, overrides)
+        @product_name = product_name
+        @style_overrides = overrides
+      end
+
+      # @return [String] path to CSS file
+      def css_file
+        File.join('/tmp', 'assets', "#{product_name}.css")
+      end
+
+      # @return [String] path to SCSS file
+      def scss_file
+        memoize(:scss_file) do
+          path = File.join('/tmp', 'assets', "#{product_name}.scss")
+          File.open(path, 'w+') do |file|
+            overrides.each do |k,v|
+              file.puts "#{k}: #{v};"
+            end
+            file.puts "//= application"
+          end
+        end
+      end
+
+      # Generate new CSS file
+      #
+      # @return [String] path to new style asset
+      def compile
+        manifest = Sprockets::Manifest.new(Rails.application.assets.dup, '/tmp/assets/compiled', Rails.application.config.assets.manifest)
+        manifest.clobber
+        manifest.environment.append_path('/tmp/assets')
+        manifest.compile(scss_file)
+        FileUtils.mv(File.join('/tmp/assets/compiled', manifest.files.keys.first), css_file)
+        css_file
+      end
+
+    end
+
+  end
+end
