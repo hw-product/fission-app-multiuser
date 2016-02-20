7bada16034c89f99299923c51fac8756c4bc0d25
Chris Roberts <code@chrisroberts.org>
2016-02-20 07:54:26 -0800

    Update notification content. Support HTML and JS insertions.

--- lib/fission-app-multiuser/subscriptions/new_notifications.rb
+++ lib/fission-app-multiuser/subscriptions/new_notifications.rb
@@ -1,15 +1,23 @@
 require 'fission-app'
 
 # Notifications event hook
-FissionApp.subscribe(/^after_render/) do |*args|
+FissionApp.subscribe(/^(before|after)_render/) do |*args|
   event = ActiveSupport::Notifications::Event.new(*args)
-  if(event.payload[:account].open_notifications.count > 0 && event.payload[:response].content_type.include?('javascript'))
-    event.payload[:body].first << FissionApp.auto_popup_formatter(
-      :dom_id => 'user-menu-toggle',
-      :title => 'OMG!',
-      :content => 'YOU HAVE MAIL!',
-      :location => 'bottom',
-      :duration => 30
-    )
+  type = event.payload[:response].content_type
+  if(type.include?('javascript') || type.include?('html'))
+    if((n_count = event.payload[:account].open_notifications.count) > 0)
+      item = FissionApp.auto_popup_formatter(
+        :dom_id => 'user-menu',
+        :title => 'Notifications',
+        :content => "New notifications <a href=\"/notifications\">(#{n_count})</a>",
+        :location => 'bottom',
+        :duration => 10
+      )
+      if(type.include?('javascript'))
+        event.body.first << item
+      else
+        event.payload[:user].run_state.script_inject << item
+      end
+    end
   end
 end
