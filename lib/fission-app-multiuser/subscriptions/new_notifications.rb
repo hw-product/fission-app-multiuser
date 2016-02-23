29abcfdd6a7292150232d5afc6cdd07c32846883
Chris Roberts <code@chrisroberts.org>
2016-02-23 11:32:56 -0800

    Check payload prior to processing events

--- lib/fission-app-multiuser/subscriptions/new_notifications.rb
+++ lib/fission-app-multiuser/subscriptions/new_notifications.rb
@@ -4,20 +4,22 @@ require 'fission-app'
 FissionApp.subscribe(/^(before|after)_render/) do |*args|
   event = ActiveSupport::Notifications::Event.new(*args)
   type = event.payload[:response].content_type
-  if((type.include?('javascript') && event.payload[:body]) || type.include?('html'))
-    if((n_count = event.payload[:user].all_open_notifications.count) > 0)
-      item = FissionApp.auto_popup_formatter(
-        :dom_id => 'user-menu',
-        :title => 'Notifications',
-        :content => "New notifications <a href=\"/notifications\">(#{n_count})</a>",
-        :location => 'bottom',
-        :duration => 10,
-        :id => "new-notifications-#{event.payload[:user].all_open_notifications.order(:created_at.desc).first.id}"
-      )
-      if(type.include?('javascript'))
-        event.payload[:body].first << item
-      else
-        event.payload[:user].run_state.script_inject << item
+  if(event.payload[:user])
+    if((type.include?('javascript') && event.payload[:body]) || type.include?('html'))
+      if((n_count = event.payload[:user].all_open_notifications.count) > 0)
+        item = FissionApp.auto_popup_formatter(
+          :dom_id => 'user-menu',
+          :title => 'Notifications',
+          :content => "New notifications <a href=\"/notifications\">(#{n_count})</a>",
+          :location => 'bottom',
+          :duration => 10,
+          :id => "new-notifications-#{event.payload[:user].all_open_notifications.order(:created_at.desc).first.id}"
+        )
+        if(type.include?('javascript'))
+          event.payload[:body].first << item
+        else
+          event.payload[:user].run_state.script_inject << item
+        end
       end
     end
   end
--- lib/fission-app-multiuser/subscriptions/notifications.rb
+++ lib/fission-app-multiuser/subscriptions/notifications.rb
@@ -3,13 +3,15 @@ require 'fission-app'
 # Notifications event hook
 FissionApp.subscribe(/fission_app$/) do |*args|
   event = ActiveSupport::Notifications::Event.new(*args)
-  event_matchers = Fission::Data::Models::AppEventMatcher.all.find_all do |aem|
-    File.fnmatch(aem.pattern, event.name)
-  end
-  event_matchers.each do |aem|
-    aem.notifications.each do |notification|
-      unless(notification.accounts.include?(event.payload[:account]))
-        notification.add_account event.payload[:account]
+  if(event.payload[:account])
+    event_matchers = Fission::Data::Models::AppEventMatcher.all.find_all do |aem|
+      File.fnmatch(aem.pattern, event.name)
+    end
+    event_matchers.each do |aem|
+      aem.notifications.each do |notification|
+        unless(notification.accounts.include?(event.payload[:account]))
+          notification.add_account event.payload[:account]
+        end
       end
     end
   end
